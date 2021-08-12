82717508b9e00da1fc9141d0d94a9f345dd81f1c
AJ Pugh <pughaj@mail.uc.edu>
2021-08-12 09:25:34 -0400

    DB Config Update and API File Creation

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,12 @@
 const { MongoClient } = require('mongodb');
+
+async function main(){
 const uri = "mongodb+srv://ascender50:<Boss5234#$>@refractlabsresumeprojec.wspxn.mongodb.net/RefractLabsResumeProject?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+try {
+    await client.connect();
+} finally{
+    await client.close();
+}
+}
+main().catch(console.error);
